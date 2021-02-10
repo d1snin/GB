3970c40cb7e5e5fd7a55954844052cbb0df51494
d1snin <d1snin@aol.com>
2021-02-10 22:44:06 +0300

    clean up

--- src/Main.java
+++ src/Main.java
@@ -1,116 +1,86 @@
-import javax.sound.midi.Soundbank;
-import java.util.Scanner;
+import java.util.Scanner; //импорт пакета, который читает пользовательский ввод
 
 public class Main {
 
     public static void main(String[] args)  {
-    while(true) {
-        Scanner scan = new Scanner(System.in);
-
+    while(true) { //с помощью этого цикла выполнение не завершится
+        Scanner scan = new Scanner(System.in); //создание нового экземпляра класса
         System.out.println("Добро пожаловать!");
         System.out.println("ведите первое число с плавающей запятой!");
-
-        float float1 = scan.nextFloat();
-
+        float float1 = scan.nextFloat(); //тут пользователь вводит число с плавающей запятой
         System.out.println("Введите второе число с плавающей запятой!");
-
-        float float2 = scan.nextFloat();
-
+        float float2 = scan.nextFloat(); //тут тоже
         System.out.println("Введите третье число с плавающей запятой!");
-
-        float float3 = scan.nextFloat();
-
+        float float3 = scan.nextFloat(); //и тут
         System.out.println("Введите четвертое число с плавающей запятой!");
-
-        float float4 = scan.nextFloat();
-
-        System.out.println("Результат: " + yes(float1, float2, float3, float4)); //К заданию 3
-
+        float float4 = scan.nextFloat(); //и тут
+        System.out.println("Результат: " + yes(float1, float2, float3, float4)); //результат к  заданию 3
         System.out.println("Введите первое число");
-
-        int firstNum = scan.nextInt();
-
+        int firstNum = scan.nextInt(); //здесь пользователь вводит целое число
         System.out.println("Введите второе число");
-
-        int secondNum = scan.nextInt();
-
-        System.out.println("Результат: " + yesThisIsAnotherMethod(firstNum, secondNum));
-         //К заданию 4
-
+        int secondNum = scan.nextInt(); //тут тоже
+        System.out.println("Результат: " + yesThisIsAnotherMethod(firstNum, secondNum)); //результат к заданию 4
         System.out.println("Введите число");
-
-        int fiveNumber = scan.nextInt();
-
-        System.out.println("Результат: " + anotherMethod(fiveNumber)); //К заданию 5
-
+        int fiveNumber = scan.nextInt(); //здесь пользователь вводит целое число
+        System.out.println("Результат: " + anotherMethod(fiveNumber)); //Результат к заданию 5
         System.out.println("Введите число");
-
-        int intYes = scan.nextInt();
-
-        System.out.println(methodYes(intYes)); //К заданию 6
-
+        int intYes = scan.nextInt(); //здесь пользователь вводит целое число
+        System.out.println(methodYes(intYes)); //К заданию
         System.out.println("Введите ваше имя для показа приветствия");
-
-        String nameYes = scan.next();
-
-        methodHaha(nameYes); //К заданию 7
-
+        String nameYes = scan.next(); //здесь пользователь вводит свое имя (строку)
+        methodHaha(nameYes); //результат к заданию 7
         System.out.println("Введите год, чтобы определить високосный он или нет");
-
-        int yearForMethod = scan.nextInt();
-
-        visocosOpredelator(yearForMethod);
-
+        int yearForMethod = scan.nextInt(); //здесь вводится год (целое число)
+        methodOpr(yearForMethod); //результат к заданию 8
         System.out.println("Программа запускается заново!");
-
+    } //на этом месте за счет цикла while начинается выполнение метода main сначала
     }
-    }
 
-
-    //idk how to name this method
     static float yes(float a, float b, float c, float d) {
         float result = a * (b + (c / d));
         return result;
     }
 
     static boolean yesThisIsAnotherMethod(int a, int b) {
-        boolean bb1 = a + b > 10; //идея жалуется когда я использую "(a + b) > 10 && < 20" или я просто чет не понял
+        boolean bb1 = a + b > 10; //жалуется когда я использую "(a + b) > 10 && < 20" или я просто чет не понял
         boolean bb2 = a + b < 20;
-        if (bb1 && bb2) {
-            return true;
-        } else {
-            return false;
+        if (bb1 && bb2) { // если значения соответствуют параметрам выше
+            return true; //возвращает истину
+        } else { //иначе
+            return false; //возвращает ложь
         }
     }
     static String anotherMethod(int a) {
-        if (a <= 0) {
-            return "Это отрицательно число";
-        } else {
-            return "Это положительно число";
+        if (a <= 0) { //если a меньше или равно нулю
+            return "Это отрицательно число"; //Возвращает строку
+        } else { // иначе
+            return "Это положительно число"; //Возвращает строку
         }
     }
     static boolean methodYes(int a) {
-        if (a < 0) {
-            return true;
-        } else {
-            return false;
+        if (a < 0) { //если a меньше нуля
+            return true; //возвращает истину
+        } else { //иначе
+            return false; //возвращает ложь
         }
     }
     static void methodHaha(String name) {
         String result = "Привет, " + name + "!";
         System.out.println(result);
     }
-    static void visocosOpredelator(int year) {
+    static void methodOpr(int year) { //инициализация метода который принимает в аргументы год
+        if (year < 1) { //если год меньше единицы
+            System.out.println("Введите валидный год!");
+        }
         int year2 = year % 4;
         int year3 = year % 400;
         int year4 = year % 100;
-        if(year2 == 0 || year3 == 0 && year4 != 0) {
+        if(year2 == 0 || year3 == 0 && year4 != 0) { // если год делится нацело на 4 или если делится нацело на 400 и не делится нацело на 100 то
             System.out.println("Это високосный год!");
-        } else {
+        } else { //иначе
             System.out.println("Это не високосный год!");
         }
     }
-
     //К заданию 2:
     int i = 6;
     float f = 4.2f;
@@ -122,15 +92,3 @@ public class Main {
     char ch = 'z';
     boolean w = false;
 }
-/*TODO:
- * 1. Создать пустой проект в IntelliJ IDEA и прописать метод main(). DONE
- * 2. Создать переменные всех пройденных типов данных и инициализировать их значения. DONE
- * 3. Написать метод, вычисляющий выражение a * (b + (c / d)) и возвращающий результат,
- * где a, b, c, d – аргументы этого метода, имеющие тип float. DONE
- * 4. Написать метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false. DONE
- * 5. Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль, положительное ли число передали или отрицательное. Замечание: ноль считаем положительным числом.
- * 6. Написать метод, которому в качестве параметра передается целое число. Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
- * 7. Написать метод, которому в качестве параметра передается строка, обозначающая имя. Метод должен вывести в консоль сообщение «Привет, указанное_имя!».
- * 8. Написать метод, который определяет, является ли год високосным, и выводит сообщение в консоль. Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
- * Если выполнение задач вызывает трудности, можете обратиться к последней странице методического пособия. Для задач со * не нужно искать решение в интернете, иначе нет смысла их выполнять.
- */
