113031ecfae43ef8c7e414984f224c4933847cd1
d1snin <d1snin@aol.com>
2021-02-11 22:25:16 +0300

    clean up

--- src/Main.java
+++ src/Main.java
@@ -15,24 +15,24 @@ public class Main {
                 float float3 = scan.nextFloat(); //и тут
                 System.out.println("Введите четвертое число с плавающей запятой!");
                 float float4 = scan.nextFloat(); //и тут
-                System.out.println("Результат: " + yes(float1, float2, float3, float4)); //результат к  заданию 3
+                System.out.println("Результат: " + calculator(float1, float2, float3, float4)); //результат к  заданию 3
                 System.out.println("Введите первое число");
-                int firstNum = scan.nextInt(); //здесь пользователь вводит целое число
+                int sumNumb1 = scan.nextInt(); //здесь пользователь вводит целое число
                 System.out.println("Введите второе число");
-                int secondNum = scan.nextInt(); //тут тоже
-                System.out.println("Результат: " + yesThisIsAnotherMethod(firstNum, secondNum)); //результат к заданию 4
+                int sumNumb2 = scan.nextInt(); //тут тоже
+                System.out.println("Результат: " + isSumBetween10and20(sumNumb1, sumNumb2)); //результат к заданию 4
                 System.out.println("Введите число");
-                int fiveNumber = scan.nextInt(); //здесь пользователь вводит целое число
-                System.out.println("Результат: " + anotherMethod(fiveNumber)); //Результат к заданию 5
+                int numberNegativeOrPositive = scan.nextInt(); //здесь пользователь вводит целое число
+                System.out.println("Результат: " + negativeOrPositive(numberNegativeOrPositive)); //Результат к заданию 5
                 System.out.println("Введите число");
-                int intYes = scan.nextInt(); //здесь пользователь вводит целое число
-                System.out.println(methodYes(intYes)); //К заданию
+                int negativeOrPositive = scan.nextInt(); //здесь пользователь вводит целое число
+                System.out.println(isNegativeOrPositive(negativeOrPositive)); //К заданию
                 System.out.println("Введите ваше имя для показа приветствия");
-                String nameYes = scan.next(); //здесь пользователь вводит свое имя (строку)
-                methodHaha(nameYes); //результат к заданию 7
+                String name = scan.next(); //здесь пользователь вводит свое имя (строку)
+                helloName(name); //результат к заданию 7
                 System.out.println("Введите год, чтобы определить високосный он или нет");
-                int yearForMethod = scan.nextInt(); //здесь вводится год (целое число)
-                methodOpr(yearForMethod); //результат к заданию 8
+                int year = scan.nextInt(); //здесь вводится год (целое число)
+                leapYearYesOrNo(year); //результат к заданию 8
                 System.out.println("Программа запускается заново!");
             } catch (Exception e) { //с помощью try catch мы можем словить ошибку и обработать ее, будет выведено сообщение ниже и код запустится заново.
                 System.out.println("вы ввели что то не так, попробуйте еще раз.");
@@ -40,12 +40,11 @@ public class Main {
         } //на этом месте за счет цикла while начинается выполнение метода main сначала
     }
 
-    static float yes(float a, float b, float c, float d) {
-        float result = a * (b + (c / d));
-        return result;
+    static float calculator(float a, float b, float c, float d) {
+        return a * (b + (c / d));
     }
 
-    static boolean yesThisIsAnotherMethod(int a, int b) {
+    static boolean isSumBetween10and20(int a, int b) {
         boolean bb1 = a + b > 10; //жалуется когда я использую "(a + b) > 10 && < 20" или я просто чет не понял
         boolean bb2 = a + b < 20;
         if (bb1 && bb2) { // если значения соответствуют параметрам выше
@@ -55,15 +54,15 @@ public class Main {
         }
     }
 
-    static String anotherMethod(int a) {
+    static String negativeOrPositive(int a) {
         if (a <= 0) { //если a меньше или равно нулю
-            return "Это отрицательно число"; //Возвращает строку
+            return "Это отрицательноe число"; //Возвращает строку
         } else { // иначе
-            return "Это положительно число"; //Возвращает строку
+            return "Это положительноe число"; //Возвращает строку
         }
     }
 
-    static boolean methodYes(int a) {
+    static boolean isNegativeOrPositive(int a) {
         if (a < 0) { //если a меньше нуля
             return true; //возвращает истину
         } else { //иначе
@@ -71,19 +70,16 @@ public class Main {
         }
     }
 
-    static void methodHaha(String name) {
+    static void helloName(String name) {
         String result = "Привет, " + name + "!";
         System.out.println(result);
     }
 
-    static void methodOpr(int year) { //инициализация метода который принимает в аргументы год
+    static void leapYearYesOrNo(int year) { //инициализация метода который принимает в аргументы год
         if (year < 1) { //если год меньше единицы
             System.out.println("Введите валидный год!");
         }
-        int year2 = year % 4;
-        int year3 = year % 400;
-        int year4 = year % 100;
-        if (year2 == 0 || year3 == 0 && year4 != 0) { // если год делится нацело на 4 или если делится нацело на 400 и не делится нацело на 100 то
+        if (year % 4 == 0 || year % 400 == 0 && year % 100 != 0) { // если год делится нацело на 4 или если делится нацело на 400 и не делится нацело на 100 то
             System.out.println("Это високосный год!");
         } else { //иначе
             System.out.println("Это не високосный год!");
